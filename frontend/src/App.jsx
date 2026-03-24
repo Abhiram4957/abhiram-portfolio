6a7f4162d815baa765c7c3c5cc387d53840c763c
Abhiram Gonuguntla <abhiram4957@gmail.com>
2026-03-24 15:12:12 +0530

    UI update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,20 @@ import Footer from './components/Footer.jsx';
 import Navbar from './components/Navbar.jsx';
 import Experience from './components/Experience.jsx';
 import "./App.css";
+import Snowfall from 'react-snowfall';
 
 const App = () => {
   return (
     <>
+    
       <div className="bg-gradients"></div>
       <div className="app-container">
+        <Snowfall 
+        snowflakeCount={700}
+        wind={[0, 2]}
+        color="white"
+        style={{ zIndex: -9999 }}
+      />
         <Navbar/>
         <Home/>
         <About/>
